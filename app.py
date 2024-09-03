7ef0f6388b85e532c1bd8b4215852e571964555e
PaulaRodArias <pau.r.arias@gmail.com>
2024-09-03 14:26:17 -0400

    cambio 5

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ st.header('Análisis exploratorio de datos - Modelos de autos')
 
 car_data = pd.read_csv(r'C:\Users\Lenovo\Desktop\TripleTen\Proyectos_Tripleten\Proyecto_Sprint6\TripleTen_Proyecto_6\vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
+scat_button = st.button('Construir gráfico de dispersión')
 
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
@@ -14,12 +15,15 @@ if hist_button: # al hacer clic en el botón
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
 
-    #crear diagrama de dispersión 
-    fig_2 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
-    
-
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
+if scat_button:
+    #escribir mensaje
+    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    #crear diagrama de dispersión 
+    fig_2 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    
     #mostrar gráfito de dispersión con plotly 
-    st.plotly_chart(fig2_use_container_width=True)
+    st.plotly_chart(fig_2,_use_container_width=True)
