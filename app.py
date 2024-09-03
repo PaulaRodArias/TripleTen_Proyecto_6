20afc18a24601c3d2b900aa78ddf67d457e5b661
PaulaRodArias <pau.r.arias@gmail.com>
2024-09-03 15:12:27 -0400

    cambios en títulos

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ scat_button = st.button('Construir gráfico de dispersión') #diagrama de disper
 
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches según distancia recorrida')
     
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
@@ -20,7 +20,7 @@ if hist_button: # al hacer clic en el botón
 
 if scat_button:
     #escribir mensaje
-    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches en relación precio-distancia recorrida')
 
     #crear diagrama de dispersión 
     fig_2 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
